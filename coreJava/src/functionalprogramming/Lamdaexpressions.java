02cc1f7f610bdbc467fd142322602de2f115fe91
rajnish <sinharajnish03@gmail.com>
2022-10-18 15:10:17 +0530

    lamda

--- coreJava/src/functionalprogramming/Lamdaexpressions.java
+++ coreJava/src/functionalprogramming/Lamdaexpressions.java
@@ -3,7 +3,7 @@ package functionalprogramming;
 public class Lamdaexpressions {
 	public static void main (String[]args)
 	{
-Addnumbers addnumber = (int a, int b)->3+4;
+Addnumbers addnumber = (int a, int b)->a+b;
 //addnumber.addNumber(3,4);
 System.out.println(addnumber.addNumber(3,4));
 
